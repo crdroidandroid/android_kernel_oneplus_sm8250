b562372fc2e93c4b0f3125e4dc72ab14252f8fe5
MattOfTheDead <matthew.slezak@gmail.com>
2021-07-18 16:59:28 -0500

    Add default wakelock block list for low drain
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,8 +16,8 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				""
+#define LIST_WL_DEFAULT				"qcom_rx_wakelock;998000.qcom,qup_uart;bluetooth_timer;hal_bluetooth_lock;smp2p-sleepstate;wlan;wlan_pno_wl;fastcg_wake_lock;c440000.qcom,spmi:qcom,pm8150b@2:qcom,qpnp-smb5;bq_delt_soc_wake_lock;alarmtimer"
 
 #define LENGTH_LIST_WL				255
-#define LENGTH_LIST_WL_DEFAULT		100
+#define LENGTH_LIST_WL_DEFAULT		255
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
