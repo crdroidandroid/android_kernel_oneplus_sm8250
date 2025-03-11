0f827dfc846cf8343378c84092a9340d8916882b
Shobhit Singh <iam@shobhit.win>
2025-03-11 09:12:53 +0000

    KernelSU: undefined path_umount error fix

--- fs/internal.h
+++ fs/internal.h
@@ -78,6 +78,10 @@ extern int sb_prepare_remount_readonly(struct super_block *);
 
 extern void __init mnt_init(void);
 
+#ifdef CONFIG_KSU
+int path_umount(struct path *path, int flags);
+#endif
+
 extern int __mnt_want_write_file(struct file *);
 extern void __mnt_drop_write_file(struct file *);
 
