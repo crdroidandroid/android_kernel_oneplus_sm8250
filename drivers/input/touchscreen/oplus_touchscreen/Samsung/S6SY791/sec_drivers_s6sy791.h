99bff31aef0d3741606aac35641ac59a0a052ff6
cjh1249131356 <cjh1249131356@gmail.com>
2025-03-11 14:50:17 +0000

    oplus_touchscreen: Forward port oos_edge_limit_support from OOS 12 driver * Stock OOS kernel still have these nodes available. Don't know why oplus deleted these codes from public source.   It's useful for users who play games much. Let's add them back.
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/input/touchscreen/oplus_touchscreen/Samsung/S6SY791/sec_drivers_s6sy791.h
+++ drivers/input/touchscreen/oplus_touchscreen/Samsung/S6SY791/sec_drivers_s6sy791.h
@@ -13,6 +13,7 @@
 #include <linux/notifier.h>
 #endif
 #include "../sec_common.h"
+#include "../../touchpanel_prevention.h"
 
 /*********PART2:Define Area**********************/
 #define GESTURE_DOUBLECLICK                     0x00
@@ -312,4 +313,9 @@ struct chip_data_s6sy791 {
 #endif //end of CONFIG_OPLUS_TP_APK
 };
 
+struct sec_support_grip_zone {
+    char                            name[GRIP_TAG_SIZE];
+    int                             (*handle_func) (struct grip_zone_area *grip_zone, bool enable);
+};
+ 
 #endif
