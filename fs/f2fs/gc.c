2ee4859fa0abe9fd1a81cebf7d780141fa2374f5
Shobhit Singh <iam@shobhit.win>
2026-01-01 13:41:11 +0000

    Revert "f2fs: set ioprio of GC kthread to idle"
    This reverts commit 2ff9be33f2eb511316f17e08ce2199e8a4fd99f6.

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -197,11 +197,10 @@ int f2fs_start_gc_thread(struct f2fs_sb_info *sbi)
 
 		kfree(gc_th);
 		sbi->gc_thread = NULL;
+		return err;
 	}
-	set_task_ioprio(sbi->gc_thread->f2fs_gc_task,
-			IOPRIO_PRIO_VALUE(IOPRIO_CLASS_IDLE, 0));
-out:
-	return err;
+
+	return 0;
 }
 
 void f2fs_stop_gc_thread(struct f2fs_sb_info *sbi)
